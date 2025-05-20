7c6577ff047187f0fe0911243ea1e67f95addee4
moji5785 <mojtabanejati79@gmail.com>
2025-05-20 09:32:27 +0330

    chore:initial project setup with Spring Boot and PostgreSQL

---
+++ src/main/java/com/mojtaba/superapp/superapp_shop/SuperappShopApplication.java
@@ -0,0 +1,13 @@
+package com.mojtaba.superapp.superapp_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperappShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mojtaba/superapp/superapp_shop/SuperappShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojtaba.superapp.superapp_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperappShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
